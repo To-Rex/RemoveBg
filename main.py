ba813fafd2fc648ebf20ff99686d1e175ee3522e
Dilshodjon <torex.amaki@gmail.com>
2023-09-04 21:46:01 +0500

    first commit

--- main.py
+++ main.py
@@ -1,29 +1,31 @@
-import uuid
 import os
+import uuid
 from flask import Flask, request, send_file, jsonify
 from PIL import Image
 from rembg import remove
-from waitress import serve
 
 app = Flask(__name__)
 
+# Construct the absolute path to the 'images' directory
+images_directory = os.path.join(os.path.dirname(__file__), 'images')
 
-def crop_image(file, width, height, output_filename):
+
+def crop_and_save_image(file, width, height, output_filename):
     try:
         image = Image.open(file)
-        # Get the dimensions of the image
         img_width, img_height = image.size
 
-        # Calculate the coordinates for cropping the center
+        # Calculate cropping coordinates for the center
         left = (img_width - width) // 2
         top = (img_height - height) // 2
         right = (img_width + width) // 2
         bottom = (img_height + height) // 2
+
         cropped_image = image.crop((left, top, right, bottom))
 
         # Generate a random filename
         filename = str(uuid.uuid4()) + '.jpg'
-        save_path = os.path.join('./images/', filename)
+        save_path = os.path.join(images_directory, filename)
         cropped_image.save(save_path)
 
         return save_path
@@ -34,52 +36,54 @@ def crop_image(file, width, height, output_filename):
 
 @app.route('/upload', methods=['POST'])
 def upload():
-    # Check if the request contains a file
     if 'file' not in request.files:
         return jsonify({'error': 'No file selected'}), 400
 
     file = request.files['file']
     remove_bg = request.form.get('remove')
 
-    if file.filename == '':
+    if not file.filename:
         return jsonify({'error': 'No file selected'}), 400
 
-    # Check if the file extension is allowed
-    if not file.filename.endswith(('.jpg', '.png', '.jpeg')):
+    if not file.filename.lower().endswith(('.jpg', '.png', '.jpeg')):
         return jsonify({'error': 'File type not allowed'}), 401
 
-    #if image height or width is less than 600px zoom it to 600px
-
-    cropped_file_path = crop_image(file, 600, 600, 'crop_img.jpg')
+    cropped_file_path = crop_and_save_image(file, 600, 600, 'crop_img.jpg')
 
     if remove_bg == 'true':
-        # Remove background using rembg library
         try:
-            bg_removed_filename = str(uuid.uuid4()) + '.png'
             with open(cropped_file_path, 'rb') as img_file:
                 output_file = remove(img_file.read())
-                bg_removed_file_path = os.path.join('./images/', bg_removed_filename)
+                bg_removed_filename = str(uuid.uuid4()) + '.png'
+                bg_removed_file_path = os.path.join(images_directory, bg_removed_filename)
                 with open(bg_removed_file_path, 'wb') as f:
                     f.write(output_file)
+
             os.remove(cropped_file_path)
-            rem = send_file(bg_removed_file_path, as_attachment=True)
-            os.remove(bg_removed_file_path)
-            return rem
+            return send_file(bg_removed_file_path, as_attachment=True)
         except Exception as e:
             print(str(e))
             return jsonify({'error': 'Error while removing background'}), 500
 
     if cropped_file_path:
-        send_files = send_file(cropped_file_path, as_attachment=True)
-        os.remove(cropped_file_path)
-        return send_files
+        try:
+            return send_file(cropped_file_path, as_attachment=True)
+        finally:
+            print("ssasaas")
+            #os.remove(cropped_file_path)
+            if os.path.exists(cropped_file_path):
+                os.remove(cropped_file_path)
+            else:
+                print("The file does not exist")
+
     else:
         return jsonify({'error': 'Error while cropping the image'}), 500
+
 
 @app.route('/')
 def index():
     return "Hello World"
 
-if __name__ == '__main__':
-    serve(app, host='0.0.0.0', port=8081)
 
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8081, debug=True)
